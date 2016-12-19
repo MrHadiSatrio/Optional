faae48220be5b29fd44b88d77e28867edf79a7b6
Hadi Satrio <hi@hadisatrio.com>
2016-12-19 20:28:12 +0700

    Add OptionalLong and its functional interfaces

---
+++ src/main/java/com/hadisatrio/optional/OptionalLong.java
@@ -0,0 +1,141 @@
+/*
+ *     Copyright (C) 2016 Hadi Satrio
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in all
+ *     copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *     SOFTWARE.
+ */
+
+package com.hadisatrio.optional;
+
+import com.hadisatrio.optional.function.Function;
+import com.hadisatrio.optional.function.LongConsumer;
+import com.hadisatrio.optional.function.LongSupplier;
+import com.hadisatrio.optional.function.Supplier;
+
+import java.io.Serializable;
+
+public final class OptionalLong implements Serializable {
+
+    private static final OptionalLong ABSENT = new OptionalLong();
+
+    private final boolean isPresent;
+    private final long value;
+
+    private OptionalLong() {
+        this.isPresent = false;
+        this.value = 0;
+    }
+
+    private OptionalLong(long value) {
+        this.isPresent = true;
+        this.value = value;
+    }
+
+    public static OptionalLong of(long value) {
+        return new OptionalLong(value);
+    }
+
+    public static OptionalLong absent() {
+        return ABSENT;
+    }
+
+    public boolean isPresent() {
+        return isPresent;
+    }
+
+    public long get() {
+        if (isPresent()) {
+            return value;
+        }
+        throw new IllegalStateException("Value is absent.");
+    }
+
+    public long or(long other) {
+        return isPresent() ? value : other;
+    }
+
+    public long or(LongSupplier otherSupplier) {
+        if (otherSupplier == null) {
+            throw new IllegalArgumentException("null may not be passed as an argument.");
+        }
+
+        return isPresent() ? value : otherSupplier.get();
+    }
+
+    public <X extends Throwable> long orThrow(X throwable) throws X {
+        if (throwable == null) {
+            throw new IllegalArgumentException("null may not be passed as an argument.");
+        }
+
+        if (isPresent()) {
+            return value;
+        }
+        throw throwable;
+    }
+
+    public <X extends Throwable> long orThrow(Supplier<? extends X> throwableSupplier) throws X {
+        if (throwableSupplier == null) {
+            throw new IllegalArgumentException("null may not be passed as an argument.");
+        }
+
+        if (isPresent()) {
+            return value;
+        }
+        throw throwableSupplier.get();
+    }
+
+    public void ifPresent(LongConsumer consumer) {
+        if (isPresent()) {
+            consumer.consume(value);
+        }
+    }
+
+    public void ifPresentOrElse(LongConsumer consumer, Function function) {
+        if (isPresent()) {
+            consumer.consume(value);
+        } else {
+            function.call();
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof OptionalLong)) {
+            return false;
+        }
+
+        final OptionalLong other = (OptionalLong) object;
+        return (isPresent() && other.isPresent())
+                ? get() == other.get()
+                : isPresent() == other.isPresent();
+    }
+
+    @Override
+    public int hashCode() {
+        return isPresent() ? new Long(value).hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return isPresent() ? String.format("OptionalLong[%s]", value) : "OptionalLong.ABSENT";
+    }
+}
